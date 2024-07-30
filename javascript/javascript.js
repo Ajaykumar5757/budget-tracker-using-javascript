c51096fe2af623344384bc2ec6beaa5e59ee1690
Ajaykumar A <ajaykumarak2703@gmail.com>
2024-07-30 22:59:16 +0530

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,14 @@
-function add(){
+function addTrans(){
     var type=document.getElementById("type").value;
     var name=document.getElementById("name").value;
     var amount=document.getElementById("amount").value;
+    var tableBody=document.getElementById("transactionTable");
+    
+    const row1=document.createElement('tr');
+    row1.innerHTML=`<td>${type}</td><td>${name}</td><td>${amount}</td><td><button id='btn'>delete</button></td>`
+    tableBody.appendChild(row1);
 
-    document.getElementsByClassName("new")[0].innerHTML=type;
-    document.getElementsByClassName("new")[1].innerHTML=name;
-    document.getElementsByClassName("new")[2].innerHTML=amount;
-    document.getElementById("new").innerHTML="<button onclick=del(this)>delete</button>"
+    row1.querySelector("#btn").addEventListener('click',()=>{
+        row1.remove();
+    });
 }
